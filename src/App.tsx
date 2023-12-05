23759acd7f951b50183bb486d7e620ac75353ab9
Hexanima <nicolasfilippelli22@gmail.com>
2023-12-05 19:00:41 -0300

    Day 3 START

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import List, { Gift } from "./components/List";
 import "./styles.css";
 
 export default function App() {
-  let giftList: Gift[] = [
-    { name: "Pelota" },
-    { name: "Muñeca" },
-    { name: "PS5" },
-  ];
   return (
     <div className="App">
-      <div className="Container">
         <h1>Regalos:</h1>
-        <List list={giftList} />
-      </div>
     </div>
   );
 }
