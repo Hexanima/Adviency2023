239ed7166a1e91de201d511ab93b9ce39561b75f
Hexanima <nicolasfilippelli22@gmail.com>
2023-12-08 21:48:45 -0300

    Day 5 START

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import "./App.scss";
-import { GiftProvider } from "./context/GiftProvider";
-import Home from "./pages/Home";
 
 function App() {
   return (
-    <GiftProvider>
-      <div className="App">
-        <Home />
-      </div>
-    </GiftProvider>
+    <div className="App">
+      <h1>Regalos:</h1>
+    </div>
   );
 }
 
--- src/hooks/useGift.ts
+++
@@ -1,8 +0,0 @@
-import { GiftContext } from "../context/GiftProvider";
-import { useContext } from "react";
-
-function useGift() {
-  return useContext(GiftContext);
-}
-
-export default useGift;
